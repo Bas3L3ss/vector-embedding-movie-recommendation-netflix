3705aa16b1596713142ce6f6b66a91c0f8054b0a
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-03-31 09:47:47 +0700

    enahnce: added router.push

--- src/components/hero-banner-cta.tsx
+++ src/components/hero-banner-cta.tsx
@@ -6,6 +6,7 @@ import { Movie } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { User } from "@supabase/supabase-js";
 import useToggleFavorite from "@/hooks/use-toggle-favorite";
+import Link from "next/link";
 
 const HeroBannerCta = ({
   film,
@@ -25,22 +26,26 @@ const HeroBannerCta = ({
   );
   return (
     <div className="flex space-x-3">
-      <Button
-        size="lg"
-        className="bg-white hover:bg-white/90 text-black font-semibold"
-        onClick={() => router.push(`/film/${film.id}`)}
-      >
-        <Play className="mr-2 h-5 w-5" />
-        Play
-      </Button>
-
-      <Button
-        size="lg"
-        variant="outline"
-        className="bg-gray-500/40 hover:bg-gray-500/60 text-white border-0 gap-2 px-6 rounded-md"
-      >
-        <Info className="w-5 h-5" /> More Info
-      </Button>
+      <Link href={`/film/${film.id}`}>
+        <Button
+          size="lg"
+          className="bg-white hover:bg-white/90 text-black font-semibold"
+          onClick={() => router.push(`/film/${film.id}`)}
+        >
+          <Play className="mr-2 h-5 w-5" />
+          Play
+        </Button>
+      </Link>
+      <Link href={`/film/${film.id}`}>
+        <Button
+          size="lg"
+          variant="outline"
+          className="bg-gray-500/40 hover:bg-gray-500/60 text-white border-0 gap-2 px-6 rounded-md"
+          onClick={() => router.push(`/film/${film.id}`)}
+        >
+          <Info className="w-5 h-5" /> More Info
+        </Button>
+      </Link>
       <Button
         size="icon"
         variant="outline"
